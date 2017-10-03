386b92f73bbb515a6243dc9bbd22e9a308f8ad52
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-03 16:20:06 -0600

    print ip address in binary

--- ip_to_bin.py
+++ ip_to_bin.py
@@ -4,7 +4,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("ip", help="ip address to convert", type=str)
     args = parser.parse_args()
-    print(args.ip)
+    ip = args.ip
+    octets = list(map(int, ip.split('.')))
+    print("({})d = ({:08b}.{:08b}.{:08b}.{:08b})b".format(ip,
+                                                octets[0],
+                                                octets[1],
+                                                octets[2],
+                                                octets[3]))
 
 
 if __name__ == '__main__':
