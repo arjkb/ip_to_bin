7a401ababf419525059bcb9af652df0d4db57244
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-10-03 16:31:37 -0600

    Output just ip in binary. No frills!

--- ip_to_bin.py
+++ ip_to_bin.py
@@ -6,12 +6,11 @@ def main():
     args = parser.parse_args()
     ip = args.ip
     octets = list(map(int, ip.split('.')))
-    print("({})d = ({:08b}.{:08b}.{:08b}.{:08b})b".format(ip,
-                                                octets[0],
+
+    print("{:08b}.{:08b}.{:08b}.{:08b}".format( octets[0],
                                                 octets[1],
                                                 octets[2],
                                                 octets[3]))
-
 
 if __name__ == '__main__':
     main()
